ea610a617588fc33288da9843171a1e71906c3b3
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-07 22:10:02 -0500

    Group routes to auth middleware

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,25 @@ use Laravel\Passport\Http\Controllers\ClientController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+    Route::post('api/uploadPhoto', 'PhotoUploadController@store')->name('home');
+
+    Route::livewire('users', 'user.users');
+    Route::livewire('profile', 'profile.profile')->name('profile');
+
+
+    Route::group(['layout' => 'layouts.settings', 'section' => 'setting'], function () {
+        Route::livewire('settings/profile', 'settings.profile')->name('settings.profile');
+        Route::livewire('settings/account', 'settings.account')->name('settings.account');
+    });
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('api/uploadPhoto', 'PhotoUploadController@store')->name('home');
-Route::redirect('settings', 'profile');
 
-Route::livewire('users', 'user.users');
-Route::livewire('profile', 'profile.profile')->name('profile');
-
-
-Route::group(['layout' => 'layouts.settings', 'section' => 'setting'], function () {
-    Route::livewire('settings/profile', 'settings.profile')->name('settings.profile');
-    Route::livewire('settings/account', 'settings.account')->name('settings.account');
-});
+Route::redirect('settings', url('settings/profile'));
