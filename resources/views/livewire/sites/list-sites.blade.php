d84091c7c490d76b872a03dffb7d1667ef64edb8
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-18 23:25:32 -0500

    Nest code

--- resources/views/livewire/sites/list-sites.blade.php
+++ resources/views/livewire/sites/list-sites.blade.php
@@ -1,5 +1,5 @@
 <div class="grid sm:grid-cols-2 container">
-@foreach($sites as $site)
-<livewire:sites.site-component :site="$site" :key="$site->id">
-@endforeach
+    @foreach($sites as $site)
+        <livewire:sites.site-component :site="$site" :key="$site->id">
+    @endforeach
 </div>
