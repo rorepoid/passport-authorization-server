8ac76df84e520264422b9406578702704a1c3d75
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-19 23:13:18 -0500

    Add pivot relation to User and Site models

--- app/Site.php
+++ app/Site.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Site extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
