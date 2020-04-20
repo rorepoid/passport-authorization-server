f5b23ae146b07dd55f05ab90e41eb6e51596fe5f
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-19 23:10:54 -0500

    Add test new sites and users have not relation

--- tests/Feature/SiteUserPivotTableTest.php
+++ tests/Feature/SiteUserPivotTableTest.php
@@ -2,21 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use App\Site;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SiteUserPivotTableTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testNewUserDoesNotBelongToAnySite()
+    {
+        $user = factory(User::class)->create();
+        $this->assertEmpty($user->sites);
+    }
+
+    public function testNewSiteDoesNotBelongToAnyUser()
+    {
+        $site = factory(Site::class)->create();
+        $this->assertEmpty($site->users);
     }
 }
