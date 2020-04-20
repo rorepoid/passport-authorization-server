f5e20706bc16ccdb3c7ca12fc0c2ec51b544ddb2
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-19 23:57:06 -0500

    Add test user can belongs to many sites

--- app/User.php
+++ app/User.php
@@ -42,6 +42,6 @@ class User extends Authenticatable
 
     public function sites()
     {
-        return $this->belongsToMany('App\Site');
+        return $this->belongsToMany('App\Site')->withTimestamps();
     }
 }
