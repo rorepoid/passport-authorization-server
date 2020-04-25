e80efb285093654f07457289449d76c371a33be7
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-25 18:32:39 -0500

    Replace Pages to Sites

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -15,7 +15,7 @@
             <div class="relative" x-data="{ open: false}">
               <a href="{{ route('sites.index') }}" class="flex flex-row items-center px-3 py-2 ml-4 text-sm font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-                <span class="mx-2">Pages</span>
+                <span class="mx-2">Sites</span>
               </a>
             </div>
             <div class="relative"">
