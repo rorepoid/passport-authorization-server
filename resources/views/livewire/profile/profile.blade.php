3f04c7a959d043fc128741acbacf87528c004a61
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-26 15:46:49 -0500

    style: Use avatar on profile card

--- resources/views/livewire/profile/profile.blade.php
+++ resources/views/livewire/profile/profile.blade.php
@@ -4,7 +4,7 @@
         <!-- Profile Card -->
             <div class="rounded-lg shadow-lg bg-gray-800 w-full flex flex-row flex-wrap p-3 antialiased">
             <div class="md:w-1/3 w-full">
-                <img class="rounded-lg shadow-lg antialiased" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">  
+                <img class="rounded-lg shadow-lg antialiased" src="{{ $user->avatar }}">
             </div>
             <div class="md:w-2/3 w-full px-3 flex flex-row flex-wrap">
                 <div class="w-full text-right text-gray-700 font-semibold relative pt-3 md:pt-0">
