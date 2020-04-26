e32c177d787a6f5de55d1d2b765d8ab12e380593
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-26 10:46:17 -0500

    style: Shadow user card an avatar on hover

--- resources/views/livewire/user/users.blade.php
+++ resources/views/livewire/user/users.blade.php
@@ -3,9 +3,9 @@
     <div class="flex items-center justify-center">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             @foreach($users as $user)
-                <div class="md:flex bg-white w-auto rounded-lg p-6 m-2 flex-wrap">
+                <div class="md:flex bg-white w-auto rounded-lg p-6 m-2 flex-wrap hover:shadow-2xl">
                     <a href="{{ route('users.show', ['id' => $user->id]) }}" class="w-full">
-                        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="https://vignette.wikia.nocookie.net/spiceandwolf/images/4/43/Horo.jpg/revision/latest?cb=20100410062559">
+                        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6 hover:shadow-2xl" src="https://vignette.wikia.nocookie.net/spiceandwolf/images/4/43/Horo.jpg/revision/latest?cb=20100410062559">
                     </a>
                     <div class="text-center md:text-left w-full">
                         <a href="{{ route('users.show', ['id' => $user->id]) }}">
