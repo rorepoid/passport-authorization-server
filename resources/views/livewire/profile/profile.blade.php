fe05e4597b7f3b0b825347cde33cadf3593dcc42
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-27 04:44:31 -0500

    feat: Show user roles on user profile card

--- resources/views/livewire/profile/profile.blade.php
+++ resources/views/livewire/profile/profile.blade.php
@@ -11,7 +11,13 @@
                 <div class="text-2xl text-white leading-tight">
                     <span>{{ $user->name }}</span>
                 </div>
-                <div class="text-normal text-gray-300 hover:text-gray-400 cursor-pointer"><span class="border-b border-dashed border-gray-500 pb-1">Administrator</span></div>
+                <div class="text-normal text-gray-300 hover:text-gray-400 cursor-pointer">
+                    @forelse($user->roles as $role)
+                        <span class="border-b border-dashed border-gray-500 pb-1 px-1 border-r-2">{{ $role->name }}</span>
+                    @empty
+                        Amazing user
+                    @endforelse
+                </div>
                 <div class="text-sm text-gray-300 hover:text-gray-400 cursor-pointer md:absolute pt-3 md:pt-0 bottom-0 right-0">We can do awesome things</div>
                 </div>
             </div>
