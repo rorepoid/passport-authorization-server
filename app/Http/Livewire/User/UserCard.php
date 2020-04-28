eb6c47c4d5a250c01055ccdc819e76e9ba78b435
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-28 18:49:19 -0500

    feat: Add content to user card component

--- app/Http/Livewire/User/UserCard.php
+++ app/Http/Livewire/User/UserCard.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\User;
 
+use App\User;
 use Livewire\Component;
 
 class UserCard extends Component
 {
+    public $user;
+
     public function render()
     {
         return view('livewire.user.user-card');
+    }
+
+    public function mount(User $user = null)
+    {
+        $this->user = $user;
     }
 }
