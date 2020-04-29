8336451c097d832504f1a5305cb46b379edd02c6
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-29 00:10:25 -0500

    test: user cannot acces to /sites route if not authenticated

--- tests/Feature/ListSitesTest.php
+++ tests/Feature/ListSitesTest.php
@@ -2,16 +2,28 @@
 
 namespace Tests\Feature;
 
+use \App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ListSitesTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testUnauthenticateUserCanNotEnterInSitesRouteAndIsRedirected()
     {
         $response = $this->get('/sites');
 
         $response->assertStatus(302);
+    }
+
+    public function testReturn403ToAccessToSitesRouteIfUserDoNotHavePermission()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->get('/sites');
+
+        $response->assertStatus(403);
     }
 }
