3480c5bcd629db1a8bc6d372a69d8a700fba5787
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-28 23:51:06 -0500

    style: Keep showing navbar buttons since md

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -10,7 +10,7 @@
         <div class="mx-3 w-full">
             <input wire:model="search" id="appsearch" class="transition-colors duration-100 ease-in-out focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ds-input" type="text" placeholder="Search the app (Press &quot;/&quot; to focus)" autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-label="search input" aria-owns="algolia-autocomplete-listbox-0" dir="auto" style="position: relative; vertical-align: top;">
         </div>
-        <div class="hidden lg:block">
+        <div class="hidden md:block">
           <div class="flex items-center">
             <div class="relative" x-data="{ open: false}">
               <a href="{{ route('sites.index') }}" class="flex flex-row items-center px-3 py-2 ml-4 text-sm font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 hover:no-underline">
