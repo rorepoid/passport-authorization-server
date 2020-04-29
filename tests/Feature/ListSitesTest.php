c83a4795dfca19211d0a41e168affd0831a3a13f
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-29 00:51:30 -0500

    test: Test admin ability to see all sites

--- tests/Feature/ListSitesTest.php
+++ tests/Feature/ListSitesTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\Sites\ListSites;
 use \App\User;
+use \App\Site;
 use Spatie\Permission\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ListSitesTest extends TestCase
@@ -37,5 +40,19 @@ class ListSitesTest extends TestCase
         $response = $this->get('/sites');
 
         $response->assertStatus(200);
+    }
+
+    public function testUserCanSeeAllSitesInSitesRouteIfIsAdmin()
+    {
+        $this->actingAs(factory(User::class)->create());
+        factory(Site::class, 5)->create();
+        $sites =Site::all();
+        Role::findOrCreate('Super Admin');
+        auth()->user()->assignRole('Super Admin');
+        foreach($sites as $site) {
+            Livewire::test(ListSites::class)
+            ->assertStatus(200)
+            ->assertSee($site->name);
+        }
     }
 }
