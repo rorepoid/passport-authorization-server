797ca8b0b1f4496c336810c24acbd43020297de8
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-29 23:59:14 -0500

    style: Center site list

--- resources/views/livewire/sites/list-sites.blade.php
+++ resources/views/livewire/sites/list-sites.blade.php
@@ -1,3 +1,4 @@
+<div class="flex justify-center">
 <div class="grid sm:grid-cols-2 container">
     @forelse($sites as $site)
         <livewire:sites.site-component :site="$site" :key="$site->id">
@@ -6,4 +7,5 @@
             <h1 class="font-bold text-center text-4xl">You don't have or belong to any site</h1>
         </div>
     @endforelse
+</div>
 </div>
