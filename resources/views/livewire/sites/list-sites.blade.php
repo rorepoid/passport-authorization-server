d8b6394e927faae6f79c0a4c04972708f1759795
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-30 00:00:23 -0500

    refactor: format code

--- resources/views/livewire/sites/list-sites.blade.php
+++ resources/views/livewire/sites/list-sites.blade.php
@@ -1,11 +1,11 @@
 <div class="flex justify-center">
-<div class="grid sm:grid-cols-2 container">
-    @forelse($sites as $site)
-        <livewire:sites.site-component :site="$site" :key="$site->id">
-    @empty
-        <div class="w-full p-3 col-span-2">
-            <h1 class="font-bold text-center text-4xl">You don't have or belong to any site</h1>
-        </div>
-    @endforelse
-</div>
+    <div class="grid sm:grid-cols-2 container">
+        @forelse($sites as $site)
+            <livewire:sites.site-component :site="$site" :key="$site->id">
+        @empty
+            <div class="w-full p-3 col-span-2">
+                <h1 class="font-bold text-center text-4xl">You don't have or belong to any site</h1>
+            </div>
+        @endforelse
+    </div>
 </div>
