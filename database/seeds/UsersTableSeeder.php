b1e3c27052e222cc68a0c935444cfac3ffab601a
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-30 23:28:36 -0500

    feat: Increase the number of fake users to create

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -35,6 +35,6 @@ class UsersTableSeeder extends Seeder
         $nat->givePermissionTo('sites');
         $nobuo->givePermissionTo('sites');
 
-        factory(User::class, 10)->create();
+        factory(User::class, 100)->create();
     }
 }
