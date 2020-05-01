2d594870ea6aa25e8a0da147156cd58296251f3a
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-04-30 23:59:34 -0500

    feat: Change button style

--- resources/views/livewire/user/users.blade.php
+++ resources/views/livewire/user/users.blade.php
@@ -22,10 +22,10 @@
                         <div class="text-gray-600">{{ $user->email }}</div>
                         <div class="text-gray-600">(555) 765-4321</div>
                         <div class="border-black mt-2 flex">
-                            <a href="/" class="flex-1 mx-1 px-2 bg-green-700 text-white rounded-lg py-1 font-bold hover:no-underline hover:bg-green-800">
+                            <a href="/" class="flex-1 mx-1 px-3 bg-green-700 text-white rounded-md py-1 font-medium hover:no-underline hover:bg-green-800">
                                 Manage
                             </a>
-                            <a href="/" class="flex-1 mx-1 px-2 bg-red-700 text-white rounded-lg py-1 font-bold hover:no-underline hover:bg-red-800">
+                            <a href="/" class="flex-1 mx-1 px-3 bg-red-700 text-white rounded-md py-1 font-medium hover:no-underline hover:bg-red-800">
                                 Delete
                             </a>
                         </div>
