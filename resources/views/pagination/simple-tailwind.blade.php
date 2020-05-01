77ac2bd60a4f0bb12999ef5e67c2ac9de7b3c38f
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-01 18:20:16 -0500

    feat: Set font-bold to next button

--- resources/views/pagination/simple-tailwind.blade.php
+++ resources/views/pagination/simple-tailwind.blade.php
@@ -23,7 +23,7 @@
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
             <li class="">
-                <a class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 select-none hover:bg-gray-200"
+                <a class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 select-none hover:bg-gray-200 font-bold"
                     href="{{ $paginator->nextPageUrl() }}"
                     rel="next"
                     aria-label="@lang('pagination.next')">
@@ -33,7 +33,7 @@
             @else
             <li aria-disabled="true" aria-label="@lang('pagination.next')">
                 <span
-                    class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-900 border-r-0 select-none"
+                    class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-900 border-r-0 select-none font-bold"
                     aria-hidden="true"
                 >Next</span>
             </li>
