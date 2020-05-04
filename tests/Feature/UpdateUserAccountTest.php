a1a4f739c520a8eb5968832ee53813643d5a524b
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-03 23:50:45 -0500

    test: Assert ok if user is authenticated

--- tests/Feature/UpdateUserAccountTest.php
+++ tests/Feature/UpdateUserAccountTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UpdateUserAccountTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function it_returns_redirect_to_login_instead_of_account_view_if_user_is_not_authenticated()
     {
@@ -15,5 +18,16 @@ class UpdateUserAccountTest extends TestCase
 
         $response->assertStatus(302);
         $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function it_returns_to_account_view_if_user_is_authenticated()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->get(route('settings.account'));
+
+        $response->assertOk();
+
     }
 }
