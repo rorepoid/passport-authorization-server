e7be6e56ff1cddd1053d12f7cc6a230db9e18906
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-04 00:10:32 -0500

    feat: Add 419 error view value

--- config/errors.php
+++ config/errors.php
@@ -19,6 +19,13 @@ return [
     /**
      * This value is the content of 404 error views
      */
+    '419' => [
+        'src' => env('ERROR_419_SRC', null),
+    ],
+
+    /**
+     * This value is the content of 404 error views
+     */
     '500' => [
         'src' => env('ERROR_500_SRC', null),
     ],
