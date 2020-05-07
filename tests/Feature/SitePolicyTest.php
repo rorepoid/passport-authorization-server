50f3976dc5f801c2270c203eb65442527398df5d
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-06 23:56:21 -0500

    test: define test to admin update sites

--- tests/Feature/SitePolicyTest.php
+++ tests/Feature/SitePolicyTest.php
@@ -2,11 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Site;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SitePolicyTest extends TestCase
 {
-    // TODO
+    /** @test */
+    function admins_can_update_sites()
+    {
+        //Arrange
+        $admin = $this->createAdmin();
+
+        $this->be($admin);
+
+        $post = new Site;
+
+        // Act
+        $result = Gate::allows('update-site', $post);
+
+        // Assert
+        $this->assertTrue($result);
+    }
 }
