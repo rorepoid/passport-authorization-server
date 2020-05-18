49ef2a28a834dc5661ac9b365f83ae981aeab403
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-18 00:58:39 -0500

    feat: Match views with auth layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('favicon.jpeg') }}" type="image/x-icon"/>
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.login', 'Login | Roa-P') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body class="overflow-hidden">
+@extends('layouts.auth')
+@section('content')
 <section class="absolute w-full h-full top-0">
     <div id="app" class=" absolute top-0 w-full h-full bg-gray-900" style="background-image: url(); background-size: 100%; background-repeat: no-repeat;"></div>
     <div class="container mx-auto px-4 h-full">
@@ -100,5 +81,4 @@
         </div>
     </div>
 </section>
-</body>
-</html>
+@endsection
