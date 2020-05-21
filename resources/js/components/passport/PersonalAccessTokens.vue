0c9176d0cf3ae22684e64773f9d847c93c6a13af
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-21 02:00:30 -0500

    feat: Add personal access token modal

--- resources/js/components/passport/PersonalAccessTokens.vue
+++ resources/js/components/passport/PersonalAccessTokens.vue
@@ -62,6 +62,106 @@
                 </div>
             </div>
         </div>
+
+        <!-- Access Token Modal -->
+        <div :class="!showModal ? ['opacity-0', 'pointer-events-none', 'modal-active'] : ''" class="my-modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
+            <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+            <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+                <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                    <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                    </svg>
+                    <span class="text-sm">(Esc)</span>
+                </div>
+
+                <!-- Add margin if you want to see some of the overlay behind the modal-->
+                <div class="modal-content py-4 text-left px-6">
+                    <!--Title-->
+                    <div class="flex justify-between items-center pb-3">
+                    <p class="text-2xl font-bold">Create Token!</p>
+                    <div class="modal-close cursor-pointer z-50">
+                        <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                        </svg>
+                    </div>
+                    </div>
+
+                    <!--Body-->
+                    <div class="modal fade" id="modal-create-token" tabindex="-1" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <!-- Form Errors -->
+                                    <div class="alert alert-danger" v-if="form.errors.length > 0">
+                                        <p class="mb-0"><strong>Whoops!</strong> Something went wrong!</p>
+                                        <br>
+                                        <ul>
+                                            <li v-for="error in form.errors">
+                                                {{ error }}
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <!-- Create Token Form -->
+                                    <form role="form" @submit.prevent="store">
+                                        <!-- Name -->
+                                        <div class="form-group row">
+                                            <label class="col-md-4 col-form-label">Name</label>
+
+                                            <div class="col-md-6">
+                                                <input id="create-token-name" type="text" class="form-control" name="name" v-model="form.name">
+                                            </div>
+                                        </div>
+
+                                        <!-- Scopes -->
+                                        <div class="form-group row" v-if="scopes.length > 0">
+                                            <label class="col-md-4 col-form-label">Scopes</label>
+
+                                            <div class="col-md-6">
+                                                <div v-for="scope in scopes">
+                                                    <div class="checkbox">
+                                                        <label>
+                                                            <input type="checkbox"
+                                                                @click="toggleScope(scope.id)"
+                                                                :checked="scopeIsAssigned(scope.id)">
+
+                                                                {{ scope.id }}
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <!-- Modal Actions -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                                    <button type="button" class="btn btn-primary" @click="store">
+                                        Create
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p>
+                        Here is your new personal access token. This is the only time it will be shown so don't lose it!
+                        You may now use this token to make API requests.
+                    </p>
+                    <textarea class="form-control" rows="10">{{ accessToken }}</textarea>
+
+                    <!--Footer-->
+                    <div class="flex justify-end pt-2">
+                        <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
+                        <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400" @click="hideCreateTokenForm">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
