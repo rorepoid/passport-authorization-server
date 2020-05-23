05ce5c1d1141e33e4539572f912ceb57de9f78ac
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-23 02:55:19 -0500

    chore: Add token list component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div x-data="modal()">
-    <button x-on:click="open">Open</button>
-    <livewire:oauth.create-personal-access-token>
+<div>
+    <div class="lg:w-1/2">
+        <div class="bg-blue-700 p-3 rounded-t-md">
+            <div >
+                <div class="grid sm:grid-cols-2">
+                    <div class="col-span-1 flex flex-row">
+                        <span class="text-white text-xl w-full sm:w-auto text-center font-bold">
+                            Personal Access Tokens
+                        </span>
+                    </div>
+                    <div class="col-span-1 flex sm:flex-row-reverse">
+                        <button
+                        class="flex-row-reverse w-full sm:w-auto text-white bg-green-700 p-2 rounded-md border-black"
+                        tabindex="-1"
+                        @click="open">
+                            Create New Token
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-gray-300 p-3 rounded-b-md">
+            <!-- No Tokens Notice -->
+            <p v-if="tokens.length === 0">
+                You have not created any personal access tokens.
+            </p>
+
+            <!-- Personal Access Tokens -->
+            <table class="w-full" v-if="tokens.length > 0">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">Name</th>
+                        <th class="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="token in tokens" class="bg-gray-400 border-8 border-gray-600">
+                        <!-- Client Name -->
+                        <td class="text-lg px-4 py-2">
+                        </td>
+
+                        <!-- Delete Button -->
+                        <td class="px-4 py-2">
+                            <button class="bg-red-700 rounded px-3 text-white" @click="revoke(token)">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+
 <script>
     function modal() {
         return {
