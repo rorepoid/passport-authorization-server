a7a8712a93be5e53675a79629a4e9af4cc304331
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-23 01:47:00 -0500

    chore: Call livewire form component to create personal access token

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div x-data="dropdown()">
+    <button x-on:click="open">Open</button>
+    <livewire:oauth.create-personal-access-token>
+</div>
+<script>
+    function dropdown() {
+        return {
+            show: false,
+            open() { this.show = true },
+            close() { this.show = false },
+            isOpen() { return this.show === true },
+        }
+    }
+</script>
 @endsection
