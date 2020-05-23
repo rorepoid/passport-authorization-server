b4d58910c3197f6dfa572108f580ab1877a8b707
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-23 01:34:42 -0500

    chore: Remove vue compoents

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,4 @@
 
 @section('content')
 
-<!-- <passport-clients></passport-clients> -->
-<!-- <passport-authorized-clients></passport-authorized-clients> -->
-<passport-personal-access-tokens></passport-personal-access-tokens>
-
-
-
 @endsection
