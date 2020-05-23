65d692d3d859e83a0867e66c1337ec8bbdcaf5c3
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-23 01:44:24 -0500

    chore: Create livewire form component to create personal access tokens

---
+++ app/Http/Livewire/Oauth/CreatePersonalAccessToken.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Oauth;
+
+use Livewire\Component;
+
+class CreatePersonalAccessToken extends Component
+{
+    public function render()
+    {
+        return view('livewire.oauth.create-personal-access-token');
+    }
+}
