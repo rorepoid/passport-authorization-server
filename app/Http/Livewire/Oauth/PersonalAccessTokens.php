3422b6db3a0aae5d336795da8ff308a9b67a6479
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-05-30 23:58:07 -0500

    feat: Create personal access token component

---
+++ app/Http/Livewire/Oauth/PersonalAccessTokens.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Oauth;
+
+use Livewire\Component;
+
+class PersonalAccessTokens extends Component
+{
+    public function render()
+    {
+        return view('livewire.oauth.personal-access-tokens');
+    }
+}
