adfc40303b8fed7a7952bfff8007df2269a53b6f
Roberto Orellana <rorellanaa@ucvvirtual.edu.pe>
2020-08-03 05:07:17 -0500

    fix: Remove margin right of card user (#6)

--- resources/views/livewire/user/users.blade.php
+++ resources/views/livewire/user/users.blade.php
@@ -12,7 +12,7 @@
             @foreach($users as $user)
                 <div class="max-w-sm md:flex bg-white w-auto rounded-lg p-6 m-2 flex-wrap hover:shadow-2xl">
                     <a href="{{ route('users.show', ['id' => $user->id]) }}" class="w-full flex justify-center">
-                        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6 hover:shadow-2xl" src="{{ $user->avatar }}">
+                        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 hover:shadow-2xl" src="{{ $user->avatar }}">
                     </a>
                     <div class="text-center md:text-left w-full flex flex-col items-center">
                         <a href="{{ route('users.show', ['id' => $user->id]) }}">
