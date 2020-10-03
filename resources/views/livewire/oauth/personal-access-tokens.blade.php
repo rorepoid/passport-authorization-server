2b72c885120d53f963d618fdc43c10a6e3e40566
Roberto Orellana <rorepoid@gmail.com>
2020-10-03 15:08:04 -0500

    feat(turbolinks): Prevent cache to personal access token component (#8)

--- resources/views/livewire/oauth/personal-access-tokens.blade.php
+++ resources/views/livewire/oauth/personal-access-tokens.blade.php
@@ -144,3 +144,8 @@
         }
     }
 </script>
+
+@push('metas')
+    {{-- This element is to prevent cache in this component --}}
+    <meta name="turbolinks-cache-control" content="no-cache">
+@endpush
