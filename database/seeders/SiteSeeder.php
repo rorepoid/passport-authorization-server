38ecd66ca88d8ff96ca39094fb1b566b4c37c1b9
Roberto Orellana <rorepoid@gmail.com>
2020-10-07 19:31:46 -0500

    feat(seeder): Set site creator in user members (#34)

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -15,23 +15,24 @@ class SiteSeeder extends Seeder
      */
     public function run()
     {
-        Site::create([
+        $nat = User::where('username', 'nat')->first();
+        $github = Site::create([
             'name' => 'Github',
             'description' => 'Github xd',
-            'user_id' => optional(User::where('username', 'nat')->first(), function ($user) {
-                return $user->id;
-            }) ?? 1,
+            'user_id' => optional($nat)->id ?? 1,
             'image' => 'https://kinsta.com/es/wp-content/uploads/sites/8/2018/05/qu%C3%A9-es-github-1.png',
-            ]);
+        ]);
+        $github->users()->save($nat);
 
-            Site::create([
+
+        $nobuo = User::where('username', 'nobuo')->first();
+        $nico = Site::create([
             'name' => 'Nico Nico Douga',
             'description' => 'Japanese yt XD',
-            'user_id' => optional(User::where('username', 'nobuo')->first(), function ($user) {
-                return $user->id;
-            }) ?? 1,
+            'user_id' => optional($nobuo)->id ?? 1,
             'image' => 'https://vignette.wikia.nocookie.net/nico-nico-douga/images/2/2f/NND.jpg/revision/latest/scale-to-width-down/340?cb=20180623023209&path-prefix=es',
         ]);
+        $nico->users()->save($nobuo);
 
         Site::factory()->count(20)->create();
     }
