aa681273d16e70f69f60627ce5ed0a732c6412ec
Roberto Orellana <rorepoid@gmail.com>
2020-10-10 23:51:53 -0500

    build: Publish Englithen provider (#44)

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Styde\Enlighten\Providers\EnlightenServiceProvider::class,
 
         /*
          * Application Service Providers...
